23a8d3d6a19f9259e563f0266fb11a4475d47463
bonymatar <bony.matar@icloud.com>
2025-04-07 13:40:49 +0200

    07-04-2025

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
+const domain = "http://82.112.241.233:1338";
+
 const getAllCats = async () => {
   let final = [];
   await axios
-    .get("http://82.112.241.233:1338/api/categories", {
+    .get(`${domain}/api/categories`, {
       params: { populate: "*" },
       pagination: { pageSize: 100 },
     })
@@ -14,7 +16,7 @@ const getAllCats = async () => {
 const getAllBooks = async () => {
   let final = [];
   await axios
-    .get("http://82.112.241.233:1338/api/books", {
+    .get(`${domain}/api/books`, {
       params: { populate: "*" },
       pagination: { pageSize: 100 },
     })
@@ -26,6 +28,63 @@ const getAllBooks = async () => {
 
 getAllBooks().then((res) => {
   console.log(res);
+  document.querySelector("#topBooks").innerHTML = "";
+  res.forEach((el) => {
+    document.querySelector("#topBooks").innerHTML += `
+      <div class="swiper-slide">
+              <div class="shop-box-items style-2">
+                <div class="book-thumb center">
+                  <a href="shop-details.html?id=${el.documentId}"
+                    ><img src="${domain + el.cover.url}" alt="img"
+                  /></a>
+                  <ul class="post-box">
+                    <li>Hot</li>
+                    <li>-30%</li>
+                  </ul>
+                  <ul
+                    class="shop-icon d-grid justify-content-center align-items-center"
+                  >
+                    <li>
+                      <a href="shop-cart.html"><i class="far fa-heart"></i></a>
+                    </li>
+                    <li>
+                      <a href="shop-cart.html">
+                        <img
+                          class="icon"
+                          src="assets/img/icon/shuffle.svg"
+                          alt="svg-icon"
+                        />
+                      </a>
+                    </li>
+                    <li>
+                      <a href="shop-details.html"><i class="far fa-eye"></i></a>
+                    </li>
+                  </ul>
+                  <div class="shop-button">
+                    <a href="shop-details.html" class="theme-btn"
+                      ><i class="fa-solid fa-basket-shopping"></i> Add To
+                      Cart</a
+                    >
+                  </div>
+                </div>
+                <div class="shop-content">
+                  <h5>${el.category ? el.category.name:'-'}</h5>
+                  <h3>
+                    <a href="shop-details.html"
+                      >${el.name}</a
+                    >
+                  </h3>
+                  <ul class="price-list">
+                    <li>$${el.salePrice? el.salePrice : '-'}</li>
+                    <li>
+                      <del>$${el.price? el.price : '-'}</del>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+    `;
+  });
 });
 
 getAllCats().then((res) => {
